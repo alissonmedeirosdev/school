961f65fe73ee0d7024a2e8a18c0c58f58baf9773
Alisson Melo <alissonmelo.work@gmail.com>
2022-01-03 02:30:00 -0300

    Create the services section and the testimony section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,19 @@
 const btnOpen = document.querySelector('.btn-open')
 const btnClose = document.querySelector('.btn-close')
 const menu = document.querySelector('header .menu')
+const header = document.querySelector('header')
+const headerHeight = header.offsetHeight
+
+window.addEventListener('scroll', event => {
+  console.log('oii')
+  if (window.scrollY >= headerHeight) {
+    //scroll is greater than header height
+    header.classList.add('scroll')
+  } else {
+    // less than header height
+    header.classList.remove('scroll')
+  }
+})
 
 btnOpen.addEventListener('click', () => {
   menu.classList.add('active')
